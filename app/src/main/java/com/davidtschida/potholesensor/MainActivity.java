87702ad5f59672af7c7946a275f5450ced8677b2
David Tschida <dmtschida1@gmail.com>
2014-06-07 21:36:47 -0400

    Logging to file now works

--- app/src/main/java/com/davidtschida/potholesensor/MainActivity.java
+++ app/src/main/java/com/davidtschida/potholesensor/MainActivity.java
@@ -6,12 +6,18 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Environment;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class MainActivity extends Activity implements SensorEventListener {
@@ -22,8 +28,11 @@ public class MainActivity extends Activity implements SensorEventListener {
     private double currentX, currentY, currentZ;
     private int accuracy;
 
-    TextView x,y,z, acc, time;
+    TextView x,y,z, acc, time, file_size, file_name;
     Switch _switch;
+
+    File file;
+    BufferedWriter out;
 
     @Override
     public void onSensorChanged(SensorEvent event) {
@@ -34,7 +43,15 @@ public class MainActivity extends Activity implements SensorEventListener {
         x.setText("X:" + currentX);
         y.setText("Y:" + currentY);
         z.setText("Z:" + currentZ);
-        time.setText("Time:"+ System.currentTimeMillis());
+
+        long _time = System.currentTimeMillis();
+        time.setText("Time:"+ _time);
+
+        try {
+            out.write(_time + ", " + currentX+ ", " + currentY + ", " + currentZ+ ", " + accuracy + "\n");
+        } catch (IOException e) {
+            Log.e("MainActivity", "Unable to write to file", e);
+        }
     }
 
     @Override
@@ -57,16 +74,59 @@ public class MainActivity extends Activity implements SensorEventListener {
         acc = (TextView) findViewById(R.id.sensor_acc);
         time = (TextView) findViewById(R.id.time);
         _switch = (Switch) findViewById(R.id._switch);
+        file_size = (TextView) findViewById(R.id.file_size);
+        file_name = (TextView) findViewById(R.id.file_name);
 
         _switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked)
+
+                new AsyncTask<Void, Void, Void>() {
+
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        try {
+                            out.flush();
+                        } catch (IOException e) {
+                            Log.e("MainActivity", "Unable to flush stream", e);
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void aVoid) {
+                        file_size.setText("Size:" + file.length()/1024 + "KB");
+                    }
+                }.execute();
+
+                if(isChecked) {
                     sensorManager.registerListener(MainActivity.this, sensor, SensorManager.SENSOR_DELAY_GAME);
-                else
+                }
+                else {
                     sensorManager.unregisterListener(MainActivity.this);
+                }
             }
         });
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        File root = Environment.getExternalStorageDirectory();
+        file = new File(root, "Sensor_log" + System.currentTimeMillis() + ".txt");
+
+        FileWriter filewriter = null;
+        try {
+            filewriter = new FileWriter(file, true);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Unable to create writer", e);
+            finish();
+        }
+        if(filewriter != null)
+            out = new BufferedWriter(filewriter);
+
+        file_name.setText(file.getName());
     }
 
     @Override
@@ -85,21 +145,27 @@ public class MainActivity extends Activity implements SensorEventListener {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void onStop()
+    {
+        super.onStop();
+        try {
+            out.close();
+        } catch (IOException e) {
+            Log.e("MainActivity", "Unable to close writer", e);
+            finish();
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+    private void writeLine(Object... elements)
+    {
+        String s = "";
+        for(int i = 0; i < elements.length ; i++)
+        {
+            s = s + elements[i].toString();
+            if(i < elements.length -1)
+                s = s + ",";
         }
-        return super.onOptionsItemSelected(item);
+
+
     }
 }
